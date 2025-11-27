3fe3c526967946f1a412d47c47938ae6d6f8ad5f
Duvan-Ang <nikogaymer37@gmail.com>
2025-11-26 20:27:29 -0500

    Spinner mejorado

--- src/pages/components/Spinner.js
+++ src/pages/components/Spinner.js
@@ -8,8 +8,17 @@ function Spinner() {
             text: 'Por favor espera.',
             allowOutsideClick: false,
             allowEscapeKey: false,
+            background: '#000000', // 🔹 Fondo del modal negro
+            color: '#ffffff',      // 🔹 Texto blanco
+            showConfirmButton: false,
             didOpen: () => {
                 Swal.showLoading();
+
+                // 🔹 Fondo de toda la pantalla (overlay) negro
+                const swalOverlay = document.querySelector('.swal2-container');
+                if (swalOverlay) {
+                    swalOverlay.style.backgroundColor = 'rgba(0,0,0,1)'; // negro sólido
+                }
             }
         });
 
@@ -22,4 +31,3 @@ function Spinner() {
 }
 
 export default Spinner;
-
